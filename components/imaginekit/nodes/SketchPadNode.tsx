7dd6e693933f879653598ff3b8f7fac392c8bb40
bashybaranaba <bashybaranaba@gmail.com>
2024-09-02 21:25:24 +0300

    feat: add toolbar in sketchpadnode

--- components/imaginekit/nodes/SketchPadNode.tsx
+++ components/imaginekit/nodes/SketchPadNode.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PencilLine } from "lucide-react";
 import BaseNode from "@/components/BaseNode"; // Import BaseNode
+import { Pencil, Eraser, Undo, Redo } from "lucide-react";
 
 interface SketchPadNodeProps {
   data: {
@@ -28,7 +29,15 @@ const SketchPadNode: React.FC<SketchPadNodeProps> = ({ data, id }) => {
       onRemoveNode={onRemoveNode}
     >
       <div className="w-full p-2">
-        <div className="relative bg-gray-50 flex flex-col items-center justify-center h-64 rounded-xl border-2">
+        <div className="flex items-center justify-center">
+          <div className=" flex items-center justify-center bg-gray-100 rounded-lg p-1 m-2 border w-32 -mb-3">
+            <Pencil className="h-4 w-4 text-gray-400 mr-1.5" />
+            <Eraser className="h-4 w-4 text-gray-400 mr-1.5" />
+            <Undo className="h-4 w-4 text-gray-400 mr-1.5" />
+            <Redo className="h-4 w-4 text-gray-400" />
+          </div>
+        </div>
+        <div className=" bg-gray-50 flex flex-col items-center justify-center h-64 rounded-xl border-2">
           <PencilLine className="w-12 h-12 text-gray-400" />
           <p className="text-gray-400 text-sm">User sketch here</p>
         </div>
