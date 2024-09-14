d57b6853e91f53f05f1f276341b8356a57737e8b
bashybaranaba <bashybaranaba@gmail.com>
2024-09-14 20:28:45 +0300

    fix: add revalidation 0 to avoid caching

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,6 @@ import NextAuth from "next-auth";
 import { authOptions } from "@/lib/configs/auth/authOptions";
 
 const handler = NextAuth(authOptions);
+export const revalidate = 0;
 
 export { handler as GET, handler as POST };
