151c5c09f9e7ee6817ac0620434dae4d8a4e9684
bashybaranaba <bashybaranaba@gmail.com>
2024-09-24 23:46:48 +0300

    feat: add back to world button in world page

--- app/[username]/worlds/[id]/page.tsx
+++ app/[username]/worlds/[id]/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 import RuntimeEngine from "@/components/RuntimeEngine";
-
 import EnterWorld from "@/components/worlds/EnterWorld";
+import { Button } from "@/components/ui/button";
 import axios from "axios";
+
+import { Earth } from "lucide-react";
 
 export default function World({ params }: { params: { id: string } }) {
   const [startInteraction, setStartInteraction] = useState(false);
@@ -24,10 +27,40 @@ export default function World({ params }: { params: { id: string } }) {
   return (
     <div>
       {!loading && app && !startInteraction && (
-        <EnterWorld app={app} setStartInteraction={setStartInteraction} />
+        <div className="flex justify-center items-center h-screen">
+          <div className="absolute top-0 left-0 ">
+            <div className="flex items-center m-2">
+              <Link href="/worlds">
+                <Button variant="outline" className="items-center mr-2">
+                  <div className="flex ">
+                    <Earth className="h-5 w-5 text-indigo-500 " />
+                  </div>
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <EnterWorld app={app} setStartInteraction={setStartInteraction} />
+        </div>
       )}
       {startInteraction && (
         <div className="flex justify-center items-center h-screen">
+          <div className="absolute top-0 left-0 ">
+            <div className="flex items-center m-2">
+              <Link href="/worlds">
+                <Button variant="outline" className="items-center mr-2">
+                  <div className="flex ">
+                    <Earth className="h-5 w-5 text-indigo-500 " />
+                  </div>
+                </Button>
+              </Link>
+              <div className="flex items-center">
+                <p className="text-lg text-blue-900 font-semibold">
+                  {app.name}
+                </p>
+              </div>
+            </div>
+          </div>
+
           <div style={{ display: "flex", height: "86vh", width: "80vw" }}>
             <div className="">
               <RuntimeEngine appId={appId} />
