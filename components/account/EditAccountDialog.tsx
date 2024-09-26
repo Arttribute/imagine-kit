2f72a81a41f7e04ae4c047ea289491a32241dc75
bashybaranaba <bashybaranaba@gmail.com>
2024-09-27 01:19:19 +0300

    fix: wrap text with apostrophe in quotes to avoid eslint error

--- components/account/EditAccountDialog.tsx
+++ components/account/EditAccountDialog.tsx
@@ -116,7 +116,7 @@ export default function EditAccountDialog({ user }: { user: any }) {
         <DialogHeader>
           <DialogTitle>Edit Profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            {"Make changes to your profile here. Click save when you're done."}
           </DialogDescription>
         </DialogHeader>
         <div className="flex items-center gap-2">
