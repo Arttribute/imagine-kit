8fae0fcc517a33c5dd65633aad5d3f16303cbc4b
bashybaranaba <bashybaranaba@gmail.com>
2024-09-28 13:35:38 +0300

    feat: add beta sticker

---
+++ components/layout/BetaSticker.tsx
@@ -0,0 +1,7 @@
+export default async function BetaSticker() {
+  return (
+    <div className="z-10 flex  mt-1.5 items-center justify-center">
+      <p className="text-xs text-blue-700">beta</p>
+    </div>
+  );
+}
