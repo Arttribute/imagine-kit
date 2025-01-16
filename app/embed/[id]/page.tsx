1315cd128622762d3ea95295050149bfc364026f
bashybaranaba <bashybaranaba@gmail.com>
2025-01-16 15:29:32 +0300

    feat: get context and insert text

--- app/embed/[id]/page.tsx
+++ app/embed/[id]/page.tsx
@@ -5,7 +5,14 @@ export default function EmbedPage({ params }: { params: { id: string } }) {
   const { id: appId } = params;
 
   return (
-    <div style={{ margin: 0, padding: 0 }}>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        boxSizing: "border-box",
+        overflow: "auto", // Let it scroll if there's more content than the iFrame
+      }}
+    >
       <RuntimeEngine appId={appId} />
     </div>
   );
